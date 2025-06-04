2d2bec01fd57d4cea0f9bae18a654968767de51e
Davi Campos Ribeiro <dcr23@inf.ufpr.br>
2025-06-04 10:03:27 -0300

    changes to fit game requirements

--- Src/TokenRing/Pkg.go
+++ Src/TokenRing/Pkg.go
@@ -1,50 +1,63 @@
 package TokenRing
 
 import (
-    "log"
-    "bytes"
-    "encoding/gob"
+	"bytes"
+	"encoding/gob"
+	"log"
 )
 
 /* This struct must be aligned */
 type TokenRingPackage struct {
-    TokenBusy byte
-    Src       byte
-    Dest      byte
-    Serial    byte
-    PkgType   byte
-    Size      byte
-    Data      [DATA_SIZE]byte
-    buffer    bytes.Buffer // unexported field to use for enconding and decoding operations
+	TokenBusy byte
+	Src       byte
+	Dest      byte
+	Serial    byte
+	PkgType   byte
+	Size      byte
+	Data      [DATA_SIZE]byte
+	buffer    bytes.Buffer // unexported field to use for enconding and decoding operations
 }
 
 func (pkg *TokenRingPackage) encodeIntoDataField(s any) int {
 
-    pkg.buffer.Reset()
-    encoder := gob.NewEncoder(&pkg.buffer)
-    err := encoder.Encode(s)
-    if err != nil {
-        log.Printf("Failed to encode data [%v]", err)
-        return -1
-    }
+	pkg.buffer.Reset()
+	encoder := gob.NewEncoder(&pkg.buffer)
+	err := encoder.Encode(s)
+	if err != nil {
+		log.Printf("Failed to encode data [%v]", err)
+		return -1
+	}
 
-    pkg.Size = byte(len(pkg.buffer.Bytes()))
-    copy(pkg.Data[:], pkg.buffer.Bytes())
+	pkg.Size = byte(len(pkg.buffer.Bytes()))
+	copy(pkg.Data[:], pkg.buffer.Bytes())
 
-    return 0
+	return 0
 }
 
 func (pkg *TokenRingPackage) decodeFromDataField(s any) int {
-    pkg.buffer.Reset()
-    pkg.buffer.Write(pkg.Data[:])
+	pkg.buffer.Reset()
+	pkg.buffer.Write(pkg.Data[:])
 
-    decoder := gob.NewDecoder(&pkg.buffer)
-    err := decoder.Decode(s)
-    if err != nil {
-        log.Printf("Failed decode data field [%v]", err)
-        return -1
-    }
+	decoder := gob.NewDecoder(&pkg.buffer)
+	err := decoder.Decode(s)
+	if err != nil {
+		log.Printf("Failed decode data field [%v]", err)
+		return -1
+	}
 
-    return 0
+	return 0
 }
 
+func (client *TokenRingClient) prepareSendPkg(dest byte, msgType int, data any) int{
+	client.sendPkg.Src = client.id
+	client.sendPkg.Dest = dest
+	client.sendPkg.PkgType = byte(msgType)
+	client.serial++
+	client.sendPkg.Serial = client.serial
+	err := client.sendPkg.encodeIntoDataField(data)
+	if err != 0 {
+		log.Printf("Failed to encode data into dataField")
+		return -1 
+	}
+	return 0 
+}
--- Src/TokenRing/TokenRing.go
+++ Src/TokenRing/TokenRing.go
@@ -1,145 +1,160 @@
 package TokenRing
 
 import (
-  "bytes"
-  "encoding/gob"
-  "log"
+	"bytes"
+	"encoding/gob"
+	"log"
 )
 
 /* Package types */
 const (
-  DATA = iota
-  BOOT // Pkg used to bootstrap the ring
-  FORWARD // Pkg used to test the ring 
-  RING_COMPLETE // Pkg used to communicate the ring is complete 
+	DATA          = iota
+	BOOT          // Pkg used to bootstrap the ring
+	FORWARD       // Pkg used to test the ring
+	RING_COMPLETE // Pkg used to communicate the ring is complete
 )
 
 /* Other constants */
 const (
-  DATA_SIZE   = 128 // dunno
-  TOKEN_FREE  = 0
-  VALID_PKG   = 1
-  FORWARD_PKG = 2
+	DATA_SIZE   = 128 // dunno
+	TOKEN_FREE  = 0
+	VALID_PKG   = 1
+	FORWARD_PKG = 2
 )
 
 type TokenRingClient struct {
-  id       byte
-  serial   byte
-  ipaddr   string
+	id     byte
+	serial byte
+	ipaddr string
 
-  sock     SockDgram
+	sock SockDgram
 
-  ipAddrs  []string
+	ipAddrs []string
 
-  hasToForward bool
+	hasToForward               bool
+	waitForToken bool
 
-  sendPkg  TokenRingPackage
-  recvPkg  TokenRingPackage
+	sendPkg TokenRingPackage
+	recvPkg TokenRingPackage
 }
 
 // recv reads a package from the socket and decodes it into recvPkg.
 func (client *TokenRingClient) recv() int {
-  buffer := make([]byte, 1024)
-  ret := client.sock.Recv(buffer)
-  if ret <= 0 {
-    log.Println("TokenRingClient: failed to receive package")
-    return -1
-  }
+	buffer := make([]byte, 1024)
+	ret := client.sock.Recv(buffer)
+	if ret <= 0 {
+		log.Println("TokenRingClient: failed to receive package")
+		return -1
+	}
 
-  decoder := gob.NewDecoder(bytes.NewReader(buffer[:ret]))
-  if err := decoder.Decode(&client.recvPkg); err != nil {
-    log.Printf("TokenRingClient: failed to decode package: %v", err)
-    return -1
-  }
+	decoder := gob.NewDecoder(bytes.NewReader(buffer[:ret]))
+	if err := decoder.Decode(&client.recvPkg); err != nil {
+		log.Printf("TokenRingClient: failed to decode package: %v", err)
+		return -1
+	}
 
-  return ret
+	return ret
 }
 
 // send encodes sendPkg and writes it to the socket.
 func (client *TokenRingClient) send() int {
-  client.sendPkg.buffer.Reset()
+	client.sendPkg.buffer.Reset()
 
-  encoder := gob.NewEncoder(&client.sendPkg.buffer)
-  if err := encoder.Encode(&client.sendPkg); err != nil {
-    log.Printf("TokenRingClient: failed to encode package: %v", err)
-    return -1
-  }
+	encoder := gob.NewEncoder(&client.sendPkg.buffer)
+	if err := encoder.Encode(&client.sendPkg); err != nil {
+		log.Printf("TokenRingClient: failed to encode package: %v", err)
+		return -1
+	}
 
-  ret := client.sock.Send(client.sendPkg.buffer.Bytes())
-  if ret <= 0 {
-    log.Println("TokenRingClient: failed to send package")
-    return -1
-  }
+	ret := client.sock.Send(client.sendPkg.buffer.Bytes())
+	if ret <= 0 {
+		log.Println("TokenRingClient: failed to send package")
+		return -1
+	}
 
-  return ret
+	return ret
 }
 
 func (client *TokenRingClient) forward() int {
-  client.sendPkg = client.recvPkg
-  return client.send()
+	client.sendPkg = client.recvPkg
+	return client.send()
 }
 
 /*
 TODO Implement token logic on Send and Recv
 */
 
-func (client *TokenRingClient) Send(dest int, msgType int, data any) int {
-  client.sendPkg.PkgType = byte(msgType)
-  client.sendPkg.Dest = byte(dest)
-  client.sendPkg.Src = client.id 
-  client.sendPkg.serial = ++client.serial
-  err := client.sendPkg.encodeIntoDataField(data)
-  if err != 0 {
-    log.Printf("Failed to encode data into dataField")
-    return -1
-  }
+func (client *TokenRingClient) Send(dest byte, data any) int {
 
-  for {
-    err = client.send()
-    if err <= 0 {
-      log.Printf("Failed to send data into dataField")
-      return -1
-    }
+	if client.waitForToken {
+		client.Recv(nil)
+		client.waitForToken = true
+	}
 
-    err = client.recv()
-    if err <= 0 {
-      log.Printf("Failed to recv pkg data into dataField")
-      return err
-    }
+	client.prepareSendPkg(dest, DATA, data)
+	
+	var err int
+	for {
+		err = client.send()
+		if err <= 0 {
+			log.Printf("Failed to send data ")
+			return -1
+		}
 
-    if client.recvPkg.src == client.id && clien.recvPkg.serial == client.serial {
-      break
-    }
-  }
-  return err
+		// wait for the pkg to comeback
+		err = client.recv()
+		if err <= 0 {
+			log.Printf("Failed to recv pkg ")
+			return err
+		}
+
+		// check pkg and free token
+		if client.recvPkg.Src == client.id && client.recvPkg.Serial == client.serial {
+			client.sendPkg.TokenBusy = 0
+			err = client.send()
+			if err <= 0 {
+				log.Printf("Failed to send data ")
+				return -1
+			}
+			break
+		}
+	}
+
+	return err
 }
 
 /* Block until valid pkg for the calling machine arrives */
+/* if passed nil recv waits for the token */
 func (client *TokenRingClient) Recv(out any) {
 
-  if client.hasToForward {
-    client.forward()
-    client.hasToForward = false
-  }
+	if client.hasToForward {
+		client.forward()
+		client.hasToForward = false
+	}
 
-  for {
-    err := client.recv()
-    if err <= 0 {
-      log.Printf("Failed to receive a pk\n") 
-      continue
-    }
+	for {
+		err := client.recv()
+		if err <= 0 {
+			log.Printf("Failed to receive a pk\n")
+			continue
+		}
 
-    if client.recvPkg.Dest == client.id {
-      err = client.recvPkg.decodeFromDataField(out)  
-      if err != 0 {
-        log.Printf("Failed to decode pkg data\n") 
-        continue
-      }
-      client.hasToForward = true
-      return 
-    }
-    client.forward()
-  }
+		if out == nil {
+			if client.recvPkg.TokenBusy == 0 {
+				client.sendPkg.TokenBusy = 1
+				return
+			}
+		}
+
+		if client.recvPkg.Dest == client.id {
+			err = client.recvPkg.decodeFromDataField(out)
+			if err != 0 {
+				log.Printf("Failed to decode pkg data\n")
+				continue
+			}
+			client.hasToForward = true
+			return
+		}
+		client.forward()
+	}
 }
-
-
